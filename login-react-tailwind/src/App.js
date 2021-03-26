3acd24f1f4d783eefe3d5196f9975afa9538767e
TienNV <tien.luckyfor@gmail.com>
2021-03-26 07:45:53 +0700

    login-react-tailwind

---
+++ login-react-tailwind/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import './assets/css/dist/tailwind.css';
+
+function App() {
+  return (
+    <div>
+      <button className="btn">pPE</button>
+    </div>
+  );
+}
+
+export default App;
