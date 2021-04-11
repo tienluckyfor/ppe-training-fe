ebc7ca111dfce449f58cb4efa71e7ecaab1ee608
TienNV <tien.luckyfor@gmail.com>
2021-04-11 23:02:47 +0700

    App.js

--- login-react-tailwind/src/App.js
+++ login-react-tailwind/src/App.js
@@ -1,9 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
 import './assets/css/dist/tailwind.css';
 import React, { useState } from 'react';
-import axios from "axios";
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import axios from 'axios';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import RegisterUser from './pages/users/RegisterUser';
 import LoginUser from './pages/users/LoginUser';
+import ForgotPassword from './pages/users/ForgotPassword';
+import CreatePost from './pages/posts/CreatePost'
+import ListPost from './pages/posts/ListPost'
+import EditPost from './pages/posts/EditPost'
 
 function App() {
   return (
@@ -11,17 +17,37 @@ function App() {
       <Switch>
         <Route
           exact
-          path={`/LoginUser`}
-          component={LoginUser}
+          path={'/EditPost/:post_id'}
+          component={EditPost}
         />
         <Route
           exact
-          path={`/RegisterUser`}
+          path={'/ListPost'}
+          component={ListPost}
+        />
+        <Route
+          exact
+          path={'/CreatePost'}
+          component={CreatePost}
+        />
+        <Route
+          exact
+          path={'/RegisterUser'}
           component={RegisterUser}
         />
         <Route
           exact
-          path={`/`}
+          path={'/LoginUser'}
+          component={LoginUser}
+        />
+        <Route
+          exact
+          path={'/ForgotPassword'}
+          component={ForgotPassword}
+        />
+        <Route
+          exact
+          path={'/'}
           render={() => <Redirect to="/RegisterUser" />}
         />
       </Switch>
